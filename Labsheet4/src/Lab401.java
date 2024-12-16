3e3dec27077282e8aa19bf190ec9cc9bf600ed4e
NattakonK <jokeryulaew@gmail.com>
2024-12-16 12:41:27 +0700

    Exampleeeee

---
+++ Labsheet4/src/Lab401.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+public class Lab401 {
+
+	static String varEmail;
+	public static void main(String[] args) {
+		inputEmail();
+
+	}
+	public static void inputEmail() {
+		String ans = "";
+		do {
+			varEmail = JOptionPane.showInputDialog("Input your e-mail:");
+			if(varEmail != null && !varEmail.isEmpty()) {
+				checkEmailError(varEmail);
+			}else {
+				JOptionPane.showMessageDialog(null, "E-mail connot be empty!");
+			}
+			ans = JOptionPane.showInputDialog("Continue?(y||Y to continue)");
+			
+			
+		}while(ans != null && ans.equalsIgnoreCase("y"));
+	}
+	public static void checkEmailError(String email) {
+		
+		while(email.startsWith("@") || email.contains(" ")) {
+			email = JOptionPane.showInputDialog("Input your e-mail, again:");
+		}
+		email = email.toLowerCase();
+		JOptionPane.showMessageDialog(null, 
+				checkEmail(email)
+				?"Your e-mail is " +email
+				:"Your e-mail is not hotmail or gmail address.");
+		
+	}
+	public static boolean checkEmail(String email) {
+		
+		return email.endsWith("@gmail.com") || email.endsWith("@hotmail.com");
+	}
+}
---
+++ Labsheet4/src/Lab403.java
@@ -0,0 +1,36 @@
+import java.io.InterruptedIOException;
+import java.util.*;
+public class Lab403 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String fullName;
+		String firstName;
+		
+		System.out.println("Please enter your name, separated by a space.");
+		System.out.print(":");
+		fullName = input.nextLine();
+		
+		int space = fullName.indexOf(' ');
+		firstName = fullName.substring(0, space);
+		System.out.println(abbreviatName(fullName)+firstName);
+		
+		
+		
+		
+		
+		input.close();
+	}
+	public static String abbreviatName(String fullName) {
+		String initialletter="";
+		
+		for(int i = 0;i<fullName.length();i++) {
+			if(fullName.charAt(i) == ' ') {
+				initialletter = (initialletter+fullName.charAt(i+1)).toUpperCase();
+				initialletter = initialletter+".";
+			}
+		}
+		return initialletter;
+	}
+
+}
