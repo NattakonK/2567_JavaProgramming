9c5133f8a7e4a53c49093cbf7e4c9fff9d2d7a66
NattakonK <jokeryulaew@gmail.com>
2025-02-17 14:27:40 +0700

    EZ

---
+++ Labsheet10/src/Barista.java
@@ -0,0 +1,25 @@
+
+public class Barista {
+
+	private String name;
+	private char gender;
+	
+	public Barista(String name ,char gender) {
+		this.name = name;
+		this.gender = gender;
+		
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getGendrName() {
+		if(this.gender=='m' || this.gender=='M') {
+			return "MAIL";
+		}
+		if(this.gender=='f' || this.gender=='F'){
+			return "FEMAIL";
+		} return " ";
+	}
+}
